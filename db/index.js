de415174c9dd131d193180b25613dba17dac5adc
Aaron Smith <no-reply>
2019-05-20 20:33:27 -0500

    push

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const uri = 'mongodb://localhost:27017/restoreNpamper'
+const uri = 'mongodb://localhost:27017/BodyBeautiful'
 
 mongoose.connect(uri).then(
     () => {
